f5996a71ff87d96980bee81506f4cae457cdd7e8
Adrian Delpha <adrian.delpha@gmail.com>
2018-09-28 21:23:39 -0700

    mock authentication via localJS

--- public/js/controllers/auth-controller.js
+++ public/js/controllers/auth-controller.js
@@ -3,6 +3,23 @@
   angular
     .module('dunderMiffinApp')
     .controller('AuthController', ['$scope', '$http', function($scope, $http) {
-
+      $scope.auth = "Your Email Address";
+      $scope.msg = "";
+      $scope.user = {};
+      $scope.validateEmail = function(){
+        $scope.user = findUser($scope.auth);
+        if(!$scope.user){
+          $scope.msg = "Invalid Email";
+        }else{
+          $scope.msg = null;
+        }
+      };
+      var findUser = function(email){
+        for (var i = testApi.length - 1; i >= 0; i--) {
+          if(testApi[i].email === $scope.auth){
+            return testApi[i];
+          }
+        }
+      }
     }]);
 })();
